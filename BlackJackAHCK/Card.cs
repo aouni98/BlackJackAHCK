9094f0c0d824513da633af9cb3240d36405ef72f
ckargus <peacewhatsup@gmail.com>
2018-03-06 00:59:17 -0500

    Finshed deck and cards

--- BlackJackAHCK/Card.cs
+++ BlackJackAHCK/Card.cs
@@ -10,15 +10,18 @@ namespace BlackJackAHCK
     {
         public string suit { get; private set;}
         public string number { get; private set; }
-        public Card(string suit, string number)
+        public int value { get; set; }
+        public Card(string suit, string number, int value)
         {
             this.suit = suit;
             this.number = number;
+            this.value = value;
         }
         public Card()
         {
             suit = "";
             number = "";
+            value = 0;
         }
     }
 }
